75da91abef881ddbe8f7c28a839765e450de5e8b
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-04-22 22:03:03 -0400

    created if statements for citation and year

--- js/script.js
+++ js/script.js
@@ -13,69 +13,43 @@ project 1 - A Random Quote Generator
 
 let quotes = [
   {quote: "If you desire many things, many things will seem few",
-  source: "Benjamin Franklin",
-  citation: "",
-  year: ""
+  source: "Benjamin Franklin"
   },
   {quote: "I have nothing to offer but blood, toil, tears, and sweat",
-  source: "Winston Churchill",
-  citation: "",
-  year: ""
+  source: "Winston Churchill"
   },
   {quote: "The boisterous sea of liberty is never without a wave",
-  source: "Thomas Jefferson",
-  citation: "",
-  year: ""
+  source: "Thomas Jefferson"
   },
   {quote: "Kind words can be short and easy to speak, but their echoes are truly endless",
-  source: "Mother Teresa",
-  citation: "",
-  year: ""
+  source: "Mother Teresa"
   },
   {quote: "Labor to keep alive in your breast that little spark of celestial fire, called conscience",
-  source: "George Washington",
-  citation: "",
-  year: ""
+  source: "George Washington"
   },
   {quote: "The time is always right to do what is right",
-  source: "Martin Luther King, Jr.",
-  citation: "",
-  year: ""
+  source: "Martin Luther King, Jr."
   },
   {quote: "If you dream it, you can do it",
-  source: "Walt Disney",
-  citation: "",
-  year: ""
+  source: "Walt Disney"
   },
   {quote: "The best things about the future is that it comes only one day at a time",
-  source: "Abraham Lincoln",
-  citation: "",
-  year: ""
+  source: "Abraham Lincoln"
   },
   {quote: "Winning is not a sometime thing, You don't do things right once in a while... you do them right all the time",
-  source: "Vince Lombardi",
-  citation: "",
-  year: ""
+  source: "Vince Lombardi"
   },
   {quote: "The future belongs to those who believe in the beauty of their dreams",
-  source: "Eleanor Roosevelt",
-  citation: "",
-  year: ""
+  source: "Eleanor Roosevelt"
   },
   {quote: "A people that values its privledges above its principles soon loses both",
-  source: "Dwight Eisenhower",
-  citation: "",
-  year: ""
+  source: "Dwight Eisenhower"
   },
   {quote: "The greatest glory in living lies not in never falling, but in rising every time we fall",
-  source: "Nelson Mandela",
-  citation: "",
-  year: ""
+  source: "Nelson Mandela"
   },
   {quote: "Imagination is more important than knowledge",
-  source: "Albert Einstein",
-  citation: "",
-  year: ""
+  source: "Albert Einstein"
   },
   {quote: "So we beat on, boats against the current, borne back ceaselessly into the past",
   source: "F. Scott Fitzgerald",
@@ -83,14 +57,10 @@ let quotes = [
   year: "1925"
   },
   {quote: "Happiness is when what you think, what you say, and what you do are in harmony",
-  source: "Mahatma Gandhi",
-  citation: "",
-  year: ""
+  source: "Mahatma Gandhi"
   },
   {quote: "To be blind, but worse is to have eyes and not see",
-  source: "Helen Keller",
-  citation: "",
-  year: ""
+  source: "Helen Keller"
   }
 ];
 
@@ -122,10 +92,23 @@ function printQuote() {
   let randomQuoteObject = getRandomQuote();
 
   //Create another variable to store the HTML string. Set it equal to a string containing two <p> elements
-  let quote = `<p class="quote">${randomQuoteObject.quote}</p><br><p class="source"> ${randomQuoteObject.source} </p>`
+  let quote = `<p class="quote">${randomQuoteObject.quote}</p><p class="source"> ${randomQuoteObject.source}`
 
-  console.log(randomQuoteObject.quote)
-  console.log(randomQuoteObject.source)
+  //If the random quote object has a citation property, concatenate a <span> element with the class "citation" to the HTML string.
+
+  if ( randomQuoteObject.citation ) {
+    quote += `<span class="citation"> ${randomQuoteObject.citation} </span>`
+  }
+
+  //If the random quote object has a year property, concatenate a <span> element with the class "year" to the HTML string.
+
+  if ( randomQuoteObject.year ) {
+    quote += `<span class="year"> ${randomQuoteObject.year} </span>`
+  }
+
+  quote += `</p>`
+
+  console.log(quote)
 
 }
 
