6ed19c87543b44c6fa797446c770e9f9245ceec2
kjablonski08@gmail.com <kjablonski08@gmail.com>
2021-01-03 18:35:44 -0500

    cleaned up JS code

--- js/script.js
+++ js/script.js
@@ -32,61 +32,46 @@ function getRandomQuote() {
 
 const printQuote = function () {
 	let randomQuoteObject = getRandomQuote();
-	console.log(randomQuoteObject);
-	console.log(typeof randomQuoteObject.author);
 	let quote = `<blockquote>${randomQuoteObject.text}`;
 	if (randomQuoteObject.author != null) {
 		quote += `<p class="source"> ${randomQuoteObject.author}</p>`;
 	}
-
-	//complete the string by concatenating a closing </p> tag to the HTML string
 	quote += `</blockquote>`;
-
-	//returns the full HTML string displaying a random quote.
 	document.querySelector('.container').innerHTML = quote;
 };
-
-/***
- * click event listener for the print quote button
- ***/
 
 document
 	.getElementById('load-quote')
 	.addEventListener('click', printQuote, false);
 
-/***
- * the getRandomBackgroundColor function displays a new background color each time the user clicks the "Show another quote" button
- ***/
+/**
+ * getRandomBackgroundColor function
+ * @return {string} HTML string displaying a random color 'Red Green Blue'
+ */
 
 function getRandomBackgroundColor() {
-	//creates a random number ranging from zero to 255 (numbers for rgb)
 	function randomNum() {
 		num = Math.random() * (0, 255) - 0;
 		num = Math.floor(num);
 		return num;
 	}
 
-	//creates a variable for red, green, blue
 	let r = randomNum();
 	let g = randomNum();
 	let b = randomNum();
 
-	//returns the full HTML string displaying a random color
 	document.querySelector('body').style.backgroundColor = `rgb(${r},${g},${b})`;
 }
 
-//click event listener to generate random background color for body element on the print quote button
 document
 	.querySelector('body')
 	.addEventListener('click', getRandomBackgroundColor, false);
 
 /***
- * The quote on the page automatically updates at regular intervals.
+ * The quote on the page automatically updates at regular intervals (10seconds)
  ***/
-
-//uses setInterval() method to print a new quote to the page at regular intervals,  every 15 seconds.
 
 setInterval(function () {
 	printQuote();
 	getRandomBackgroundColor();
-}, 15000);
+}, 10000);
